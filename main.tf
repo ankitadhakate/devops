9379aeda4f02a8399f2ea57ced71d292942da0fc
omkardeshpande3194 <deshpandeomkar1997@gmail.com>
2025-11-05 23:59:09 +0530

    provider configuration

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.9.0"
+}
+
+provider "google" {
+  project = "cicd-devops-ankita"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
